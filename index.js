0f34b8e91023a4d88c6b154c20aea37ad9bc3d2d
boyiin <61267703+boyiin@users.noreply.github.com>
2020-11-27 00:05:28 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,61 @@
+var express = require("express");
+var bodyParser = require('body-parser');
+var app = express();
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.listen(3000, () => {
+ console.log("Server running on port 3000");
+});
+app.post("/task1", (req, res, next) => {
+    // Input Validation on an object
+    var data = req.body.data;
+    var rules = req.body.rules;
+
+validateBody = (requestBody, rules) => {
+  const bodyKeys = Object.keys(requestBody);
+
+  let missingItems = [];
+
+  rules.forEach((rule) => {
+    if (!bodyKeys.includes(rule)) {
+      missingItems.push(rule);
+    }
+  });
+
+  if (missingItems.length > 0) {
+    console.log("Missing Items", missingItems);
+    return missingItems;
+  } else {
+      res.json("valid");
+    console.log("Valid");
+    return "Valid";
+  }
+};
+
+const testData = {
+  type: "durban",
+  crux: "Induce",
+  color: "green",
+  title: "Idiot",
+  riot: "YYYHJ",
+};
+
+validateBody(data, rules);
+
+});
+// end of task1
+
+app.post("/task2", (req, res ) => {
+    console.log(req.body.data);
+    var data = req.body.data;
+    var useritem = req.body.item;
+    delete data[useritem];
+    res.json(data);
+    console.log(data);
+});
+// end of task2
